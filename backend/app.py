1d2f69d58bdda8a8b75dc6b31a300f8628d87cbe
Orestas41 <orestasdulinskas@gmail.com>
2024-07-23 16:18:27 +0100

    Added rotating API keys. Added topics list and made it into a random choice. Increase GIF time to 18 seconds so that both API keys will allow 1 hour span

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,27 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from flask_cors import CORS
 import requests
 import random
-import re
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
-# Your Tumblr API key
-API_KEY = '4JlRp52tQElhZnLKSBn3n4CGa5wfW7RZ'
-#API_KEY = 'fyJYc9OHmlh9Iyb271bzkFA9cGEUrcoB6FmkClVTgejQCMshfH'
+api = 0
+
+with open('./api_keys.txt', 'r') as f:
+    apis = [line.strip() for line in f]
+
+with open('./topics.txt', 'r') as f:
+    topics = [line.strip() for line in f]
+
 @app.route('/random-content')
-def random_content():    
-    search_query = 'puppies'
-    count = 1
-    search_endpoint = f'https://api.giphy.com/v1/gifs/search?api_key={API_KEY}&q={search_query}&limit={count}'
+def random_content():
+    global api
+    if api < len(apis)-1: api += 1
+    else:   api = 0
+    search_endpoint = f'https://api.giphy.com/v1/gifs/search?api_key={apis[api]}&q={random.choice(topics)}&limit=5'
     response = requests.get(search_endpoint)
     data = response.json()
-    return jsonify({'url': data['data'][0]['images']['original']['url']})
-
+    return jsonify({'url': data['data'][random.randint(0, 4)]['images']['original']['url']})
 if __name__ == '__main__':
     app.run(debug=True)
