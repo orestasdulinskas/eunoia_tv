a21941bff12249ef446313d3ff663e132f4cf6c1
Orestas41 <orestasdulinskas@gmail.com>
2024-08-28 12:57:34 +0100

    Previous API only pulled 20 posts max as limited by Tumblr API. Update backend to pull all posts from tumblr and output a list of URLs. Updated frontend to save a list provided by API and display random url every 5 seconds.

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_cors import CORS
 import re
 import requests
-import random
 import os
 import json
 from dotenv import load_dotenv
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -16,20 +17,39 @@ API_KEY = os.getenv('TUMBLR_API_KEY')
 
 @app.route('/random-content')
 def random_content():
+    all_posts = []
+    offset = 0
+    limit = 20
+
+    session = requests.Session()
+    retry = Retry(connect=5, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('https://', adapter)
+
     # Tumblr blog URL
     blog_url = 'eunoia-tv.tumblr.com'
-    # Make a request to the Tumblr API to get posts
-    response = requests.get(f'https://api.tumblr.com/v2/blog/{blog_url}/posts?api_key={API_KEY}')
-    # Parse the JSON response
-    data = response.json()
-    # Extract post URLs (assuming they are images for simplicity)
-    post_urls = [re.findall(r'src="([^"]+)"',post['body']) for post in data['response']['posts']]
-    # Choose a random post URL
-    random_url = random.choice(post_urls)
-    # Return the random post URL
+
+    while True:
+        # Make a request to the Tumblr API to get posts
+        response = session.get(f'https://api.tumblr.com/v2/blog/{blog_url}/posts?api_key={API_KEY}&offset={offset}&limit={limit}', timeout=10)
+        data = response.json()
+
+        # Check if there are posts in the response
+        if not data['response']['posts']:
+            break
+
+        # Extract post URLs (assuming they are images for simplicity)
+        post_urls = [re.findall(r'src="([^"]+)"', post['body']) for post in data['response']['posts']]
+
+        all_posts.extend(post_urls)
+
+        # Increment the offset
+        offset += limit
+
+    # Return the list of URLs
     return {
         'statusCode': 200,
-        'body': json.dumps({'url': random_url}),
+        'body': json.dumps({'url': all_posts}),
         'headers': {
             'Access-Control-Allow-Origin': '*',
             'Content-Type': 'application/json'
